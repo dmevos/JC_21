bf564ae22cc3bf01e0abfda99a9efcd9431068d1
Dmitriy Osipov <deosipov@mail.ru>
2022-05-26 21:55:05 +0300

    Реализовано решение задачи 2.1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> outList = new ArrayList<>();
+        for (int i : intList) {
+            if (i > 0 && i % 2 == 0) {
+                outList.add(i);
+            }
+        }
+        outList.sort(Comparator.naturalOrder());
+        for (int i : outList) {
+            System.out.print(i + " ");
+        }
+    }
 }
